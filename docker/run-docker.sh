8ab48dac50c74011d0b47215bf16d524a0360f9f
Mark Vejvoda <mark_vejvoda@hotmail.com>
2024-03-26 06:06:50 -0700

    - remove old docker config

--- docker/run-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -p "8080:80" -v ${PWD}/app:/app -e APP_SMTP_OutboundUsername='markvejvoda@gmail.com' -e APP_SMTP_OutboundPassword='ruxnfeaflnyalmwf' -e APP_SMTP_OutboundFromAddress='markvejvoda@gmail.com' softcoder/membership-signup:latest
